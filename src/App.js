1363abd19ee5c3549f7ec983517a42cef144ac4b
Nicolas AUGER <nicolas@poool.fr>
2019-01-23 12:11:38 +0100

    feat: init main view

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ import Header from './fragments/Header';
 export default class App extends React.Component {
   render() {
     return (
-      <BackgroundImage style={styles.container}>
+      <BackgroundImage>
         <StatusBar barStyle='light-content' />
         <Header />
+        <View style={styles.main_view}></View>
       </BackgroundImage>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#F5FCFF',
-    fontFamily: 'Gotham-Medium'
+  main_view: {
+    backgroundColor: '#2A2A2A',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#FFF',
+    fontFamily: 'Gotham-Medium',
+    height: '100%',
+    margin: 20,
+    // marginBottom: 0,
   },
 });
