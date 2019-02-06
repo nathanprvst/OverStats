da0127c716fc0a1186fd48e692b3d95c449c47a1
PREVOST Nathan <nathanp33@laposte.net>
2019-02-06 10:10:59 +0100

    feat: add form home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, StatusBar, Button, TextInput, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Formik } from 'formik';
 
 import BackgroundImage from '../fragments/BackgroundImage';
 
@@ -10,11 +11,40 @@ export default class Home extends React.Component {
       <BackgroundImage>
         <StatusBar barStyle='light-content' />
         <View style={styles.main_view}>
-            <Button
-                title="Stats"
-                onPress={() => Actions.stats()}
-            />
+          <Text style={styles.form}>
+              <Text style={styles.main_text}>
+                Renseigner le nom d’utilisateur d’un joueur
+              </Text>
+              <Text style={styles.second_text}> Overwatch </Text>
+              <Text style={styles.main_text}>
+                pour voir ses statistiques. Remplisser son pseudo dans le champ ci-dessous :
+              </Text>
+          </Text>
+          <Formik
+            initialValues={{ email: '' }}
+            onSubmit={values => console.log(values)}
+          >
+            {props => (
+              <View>
+                <TextInput
+                  onChangeText={props.handleChange('email')}
+                  onBlur={props.handleBlur('email')}
+                  value={props.values.email}
+                  style={styles.input}
+                  placeholder="Nom d'utilisateur..."
+                />
+                <TouchableOpacity
+                    onPress={() => Actions.stats()}
+                    title="Submit"
+                    style={styles.submit}
+                >
+                    <Text style={styles.submitText}>Rechercher</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </Formik>
         </View>
+
       </BackgroundImage>
     );
   }
@@ -26,9 +56,43 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     borderColor: '#FFF',
-    fontFamily: 'Gotham-Medium',
     height: '100%',
     margin: 20,
+    padding: 20,
     // marginBottom: 0,
+  },
+  main_text: {
+      color: '#FFF',
+      fontSize: 20,
+  },
+  second_text: {
+      color: '#DFAA35',
+      fontWeight: 'bold',
+      fontSize: 20,
+  },
+  form: {
+    marginBottom: 50,
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: '#DFAA35',
+    fontSize: 18,
+  },
+  submit: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: '#DFAA35',
+    fontSize: 18,
+    marginTop: 20,
+  },
+  submitText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
