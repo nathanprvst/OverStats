27276d3d600df0ab4c2f706aebf0e7ed98cfc0c8
Nicolas AUGER <nicolas@poool.fr>
2019-04-09 19:42:12 +0200

    feat: add heroes content

---
+++ src/services/content.js
@@ -0,0 +1,142 @@
+export const _heroes = {
+  "ana": {
+    "name": 'ana',
+    "color": '#718AB3',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/ana/icon-portrait.png',
+  },
+  "ashe": {
+    "name": 'ashe',
+    "color": '#676966',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/ashe/icon-portrait.png',
+  },
+  "bastion": {
+    "name": 'ana',
+    "color": '#7C8F7B',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/bastion/icon-portrait.png',
+  },
+  "brigitte": {
+    "name": 'brigitte',
+    "color": '#BE736E',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/brigitte/icon-portrait.png',
+  },
+  "doomfist": {
+    "name": 'doomfist',
+    "color": '#815049',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/doomfist/icon-portrait.png',
+  },
+  "dVa": {
+    "name": 'dVa',
+    "color": '#ED93C7',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/dva/icon-portrait.png',
+  },
+  "genji": {
+    "name": 'genji',
+    "color": '#97EF43',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/genji/icon-portrait.png',
+  },
+  "hanzo": {
+    "name": 'hanzo',
+    "color": '#B9B48A',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/hanzo/icon-portrait.png',
+  },
+  "junkrat": {
+    "name": 'junkrat',
+    "color": '#ECBD53',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/junkrat/icon-portrait.png',
+  },
+  "lucio": {
+    "name": 'lucio',
+    "color": '#85C952',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/icon-portrait.png',
+  },
+  "mccree": {
+    "name": 'mccree',
+    "color": '#AE595C',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/mccree/icon-portrait.png',
+  },
+  "mei": {
+    "name": 'mei',
+    "color": '#6FACED',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/mei/icon-portrait.png',
+  },
+  "mercy": {
+    "name": 'mercy',
+    "color": '#EBE8BB',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/mercy/icon-portrait.png',
+  },
+  "moira": {
+    "name": 'moira',
+    "color": '#803C51',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/moira/icon-portrait.png',
+  },
+  "orisa": {
+    "name": 'orisa',
+    "color": '#468C43',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/orisa/icon-portrait.png',
+  },
+  "pharah": {
+    "name": 'pharah',
+    "color": '#3E7DCA',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/pharah/icon-portrait.png',
+  },
+  "reaper": {
+    "name": 'reaper',
+    "color": '#7D3E51',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/reaper/icon-portrait.png',
+  },
+  "reinhardt": {
+    "name": 'reinhardt',
+    "color": '#929DA3',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/reinhardt/icon-portrait.png',
+  },
+  "roadhog": {
+    "name": 'roadhog',
+    "color": '#B68C52',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/roadhog/icon-portrait.png',
+  },
+  "soldier76": {
+    "name": 'soldier-76',
+    "color": '#697794',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/soldier-76/icon-portrait.png',
+  },
+  "sombra": {
+    "name": 'sombra',
+    "color": '#7359BA',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/sombra/icon-portrait.png',
+  },
+  "symmetra": {
+    "name": 'symmetra',
+    "color": '#8EBCCC',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/symmetra/icon-portrait.png',
+  },
+  "torbjorn": {
+    "name": 'torbjorn',
+    "color": '#C0726E',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/torbjorn/icon-portrait.png',
+  },
+  "tracer": {
+    "name": 'tracer',
+    "color": '#D79342',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/tracer/icon-portrait.png',
+  },
+  "widowmaker": {
+    "name": 'widowmaker',
+    "color": '#9E6AA8',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/widowmaker/icon-portrait.png',
+  },
+  "winston": {
+    "name": 'winston',
+    "color": '#A2A6BF',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/winston/icon-portrait.png',
+  },
+  "zarya": {
+    "name": 'zarya',
+    "color": '#E77EB6',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/zarya/icon-portrait.png',
+  },
+  "zenyatta": {
+    "name": 'zenyatta',
+    "color": '#EDE582',
+    "icon": 'https://d1u1mce87gyfbn.cloudfront.net/hero/zenyatta/icon-portrait.png',
+  },
+};
